6d6a1545a0396714d43737cd061f90ff7c8601ab
rikka <admin@rikka.cc>
2025-04-01 13:27:04 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red-200">
+      <h1 className="text-green-800">Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+      <span className="icon-[cryptocurrency--btc]"></span>
+    </div>
+  );
+}
+
+export default App;
