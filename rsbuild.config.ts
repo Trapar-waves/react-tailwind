e28820b5584776981009b6a617cba376baf58d6c
Rikka <admin@rikka.cc>
2026-04-27 09:26:49 +0800

    chore: add rsdoctor support and update dependencies in configuration

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,14 +1,37 @@
+import process from "node:process";
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
+import { RsdoctorRspackPlugin } from "@rsdoctor/rspack-plugin";
 import tailwind from "@tailwindcss/postcss";
 
+const enableRsdoctor = Boolean(process.env.RSDOCTOR);
+
 export default defineConfig({
+  performance: {
+    ...(enableRsdoctor ? { buildCache: false } : {}),
+  },
   plugins: [pluginReact()],
   tools: {
     postcss: {
       postcssOptions: {
         plugins: [tailwind],
       },
+    },
+    rspack: {
+      plugins: [
+        ...(enableRsdoctor
+          ? [
+              new RsdoctorRspackPlugin({
+                output: {
+                  mode: "brief",
+                  options: {
+                    type: ["json"],
+                  },
+                },
+              }),
+            ]
+          : []),
+      ],
     },
   },
 });
